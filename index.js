15167f04baff2f6358ce1bd0de6158be39c389e2
unknown <kaiserabir222@gmail.com>
2021-10-29 05:12:21 +0600

    updated

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares //
 app.use(cors());
 app.use(express.json());
+
+// setup mongoDB //
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3myda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Shift-us");
+    const serviceCollection = database.collection("services");
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("welcome to server");
